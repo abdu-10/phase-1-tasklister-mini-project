9488b1df49a1192c504f20529b603e80b1959b53
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-12-03 16:40:36 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+let form = document.getElementById("create-task-form");
+let list = document.getElementById("tasks");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newTask = document.getElementById("new-task-description");
+  createListItem(newTask.value);
+  form.reset();
+})
+
+function createListItem(listItem) {
+  let newListItem = document.createElement("li");
+  newListItem.textContent = `${listItem} `;
+
+  let newButton = document.createElement("button");
+  newButton.addEventListener("click", handleDelete)
+  newButton.textContent = "X";
+  newListItem.appendChild(newButton);
+
+  list.append(newListItem);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+};
